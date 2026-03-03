42eb38bf09d70ee1a61ca83bb2a270c1b9bb8587
0x0D <0xOD@proton.me>
2026-03-03 15:53:13 +0100

    fix: improve console.log message readability

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@ function handleClick() {
 
   // wait a random amount of seconds and make it re-appear
   var randomRespawnTime = Math.floor(Math.random() * maxRandomRespawnTime)
-  console.log(randomRespawnTime)
+  console.log("Pichu will re-appear in: " + randomRespawnTime + " seconds...")
   setTimeout(async () => {
     isVisible.value = true;
     
